554e26cae828bd0ab7a38a762aac746d0a22c40f
Murat Öztürk <45845027+ozturkmmurat@users.noreply.github.com>
2023-03-15 08:59:39 +0300

    CostVariableValidator hata veriyor.

--- Business/ValidationRules/FluentValidation/CostVariable/CostVariableValidator.cs
+++ Business/ValidationRules/FluentValidation/CostVariable/CostVariableValidator.cs
@@ -9,13 +9,13 @@ namespace Business.ValidationRules.FluentValidation.CostVariable
     {
         public CostVariableValidator()
         {
-            RuleFor(cv => cv.CostVariableName).NotEmpty().WithMessage("Maliyet adı boş bırakılamaz.").WithName("Maliyet Değişken Adı");
-            RuleFor(cv => cv.CostVariableName).MinimumLength(3).MaximumLength(20).WithMessage("Maliyet Değişkeninin adı en az 3 karakter en fazla 20 karakter olmalıdır.").WithName("Maliyet Değişken Adı");
-            RuleFor(cv => cv.IProfile).NotEmpty().WithMessage("I Profil değeri boş bırakılamaz.").WithName("I Profil");
-            RuleFor(cv => cv.ShateIron).NotEmpty().WithMessage("Saç değeri boş bırakılamaz. ").WithName("Saç");
-            RuleFor(cv => cv.FireTotalPercentAge).NotEmpty().WithMessage("Fire Toplam Ağırlık yüzdesi boş bırakılamaz.").WithName("");
-            RuleFor(cv => cv.FirePercentAge).NotEmpty().WithMessage("Fire yüzdesi boş bırakılamaz").WithName("Fire Yüzdesi");
-            RuleFor(cv => cv.FireShateIronAndIProfilePercentage).NotEmpty().WithMessage("Fireli I Profil ve Saç değeri boş bırakılamaz").WithName("");
+            //RuleFor(cv => cv.CostVariableName).NotEmpty().WithMessage("Maliyet adı boş bırakılamaz.").WithName("Maliyet Değişken Adı");
+            //RuleFor(cv => cv.CostVariableName).MinimumLength(3).MaximumLength(20).WithMessage("Maliyet Değişkeninin adı en az 3 karakter en fazla 20 karakter olmalıdır.").WithName("Maliyet Değişken Adı");
+            //RuleFor(cv => cv.IProfile).NotEmpty().WithMessage("I Profil değeri boş bırakılamaz.").WithName("I Profil");
+            //RuleFor(cv => cv.ShateIron).NotEmpty().WithMessage("Saç değeri boş bırakılamaz. ").WithName("Saç");
+            //RuleFor(cv => cv.FireTotalPercentAge).NotEmpty().WithMessage("Fire Toplam Ağırlık yüzdesi boş bırakılamaz.");
+            //RuleFor(cv => cv.FirePercentAge).NotEmpty().WithMessage("Fire yüzdesi boş bırakılamaz").WithName("Fire Yüzdesi");
+            //RuleFor(cv => cv.FireShateIronAndIProfilePercentage).NotEmpty().WithMessage("Fireli I Profil ve Saç değeri boş bırakılamaz");
         }
     }
 }
